431010dbaeef36a0d524cf4caf6de7ce11c20fc7
Ultim4tum <104349084+Ultim4tum@users.noreply.github.com>
2023-10-03 18:55:05 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Makeo</title>
+    <link rel="stylesheet" href="./style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="container_body">
+        <?php
+        include 'controleurs/controller.php';
+        ?>
+    </div>
+</body>
+</html>
